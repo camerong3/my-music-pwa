f495f35fd59559b118399b3ec54df5f28163daff
camerong3 <148712808+camerong3@users.noreply.github.com>
2024-08-13 12:45:26 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -304,8 +304,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         hasVoted = false; // Reset the voting flag for the new song
-        localStorage.removeItem('hasVoted'); // Update local storage
-        window.location.reload(); // Reload the page to update UI
+        localStorage.setInterval('hasVoted', hasVoted); // Update local storage
     }
 
     // Function to move to the next song (for group leader only)
