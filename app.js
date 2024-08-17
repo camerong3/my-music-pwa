98ea6e0ba3546cf0cef59e20a4896c0fbb978a1b
camerong3 <148712808+camerong3@users.noreply.github.com>
2024-08-17 10:23:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -202,10 +202,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (groupID && groupIDElement) {
             groupIDElement.id = 'current-group-id';
             groupIDElement.textContent = `${groupID}`;
-            document.getElementById('current-group-id').replaceChild(groupIDElement);
+            document.getElementById('current-group-id').appendChild(groupIDElement);
         } else {
             groupIDElement.textContent = 'No Group ID';
-            document.getElementById('current-group-id').replaceChild(groupIDElement);
+            document.getElementById('current-group-id').appendChild(groupIDElement);
         }
 
         // Update button text if the user is the leader
