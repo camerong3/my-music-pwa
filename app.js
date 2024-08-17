4cb06f7eb0c516cfafdabf7f0121771ffc8009d7
camerong3 <148712808+camerong3@users.noreply.github.com>
2024-08-17 14:48:05 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -514,7 +514,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Register the service worker
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/my-music-pwa/sw.js')
+            navigator.serviceWorker.register('/my-music-pwa/sw.js', { scope: '/my-music-pwa/' })
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
     
